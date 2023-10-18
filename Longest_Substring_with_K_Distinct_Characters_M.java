27c95e0063313e21d7b738d23c4bc792a20d2076
Anand <jeevaanand93002@gmail.com>
2023-10-18 19:06:50 +0530

    sliding window pattern problem, the sliding window is dynamic here

---
+++ Longest_Substring_with_K_Distinct_Characters_M.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Longest_Substring_with_K_Distinct_Characters_M {
+    
+
+
+    public static void main(String[] args) {
+        Map<Character, Integer> map = new HashMap<>();
+
+        map.put('a', 5);
+        map.put('a', 10);
+        map.put('v', 5);
+
+        System.out.println(map);
+    }
+}
